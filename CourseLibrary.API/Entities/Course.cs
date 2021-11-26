25969f4d23e491411343c2e49eaa60b2d3cc6ccb
George Maina <nuriszka@gmail.com>
2021-11-26 13:16:06 +0100

    Add project files.

---
+++ CourseLibrary.API/Entities/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace CourseLibrary.API.Entities
+{
+    public class Course
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title{ get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [JsonIgnore]
+        public Author Author { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public Guid AuthorId { get; set; }  
+    }
+}
---
+++ CourseLibrary.API/Startup.cs
@@ -0,0 +1,55 @@
+using CourseLibrary.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CourseLibrary.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CourseLibraryContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CourseLibrary.API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CourseLibrary.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
